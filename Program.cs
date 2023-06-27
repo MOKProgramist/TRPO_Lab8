1cf906f056ebe488f0a9ec5f56cb4ac91f38d5b5
quick6response <64421792+quick6response@users.noreply.github.com>
2023-06-27 20:41:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ConsoleApp1.ServiceReference1;
+
+namespace Microsoft.ServiceModel.Samples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Client WCF: Andrey R.";
+            try
+            {
+                FunctionCalculateClient functionCalculateClient = new FunctionCalculateClient();
+
+                functionCalculateClient.Open();
+
+                double value1 = 100.00D;
+                double value2 = 15.99D;
+                double result = functionCalculateClient.Number8(value1, value2);
+                Console.WriteLine($"Function 8: {result}");
+
+                double result1 = functionCalculateClient.Number20(value1, value2);
+                Console.WriteLine($"Function 22: {result1}");
+                functionCalculateClient.Close();
+
+                Console.WriteLine("Сеанс работы с сервисом завершен.");
+            } catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка подключения к службе. {ex.Message}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
